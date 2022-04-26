a08d5d4b0329b4755db52de0382a5465cfd22f9e
ThanhHung-488 <thanhhung14072001@gmail.com>
2022-04-26 08:15:07 +0700

    nop bai

---
+++ Bai2/Bai2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat();
+            cat.eat();
+            cat.makeSound();
+            cat.run();
+            Bird bird = new Bird();
+            bird.eat();
+            bird.makeSound();
+            bird.fly();
+            Console.ReadKey();
+        }
+    }
+    class Animal
+    {
+        public virtual void eat()
+        {
+            Console.WriteLine("Dong vat an co");
+        }
+        public virtual void makeSound()
+        {
+            Console.WriteLine("");
+        }
+    }
+    class Cat : Animal
+    {
+        public void run()
+        {
+            Console.WriteLine("Meo run");
+
+        }
+        public override void eat()
+        {
+            Console.WriteLine("Meo an ca");
+        }
+        public override void makeSound()
+        {
+            Console.WriteLine("Meo meo meo");
+        }
+    }
+    class Bird : Animal
+    {
+        public void fly()
+        {
+            Console.WriteLine("Chim bay tren troi");
+
+        }
+        public override void eat()
+        {
+            Console.WriteLine("Chim an sau");
+        }
+        public override void makeSound()
+        {
+            Console.WriteLine("chim keu");
+        }
+    }
+}
